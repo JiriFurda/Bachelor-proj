8fef2cb13f1e6286261857f7656ee5bb9cd1c353
Furdys <magiecz@gmail.com>
2019-01-28 01:52:51 +0100

    Extracting topic id and title

--- calls/extractor.py
+++ calls/extractor.py
@@ -2,6 +2,9 @@
 import urllib, json
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Document, Text, Index, Nested, InnerDoc
+import re
+from pprint import pprint
+
 
 client = Elasticsearch()
 callsIndex = Index('xfurda00_calls', using=client)
@@ -10,7 +13,7 @@ if callsIndex.exists():
     callsIndex.delete(ignore=404)
 
 class Topic(InnerDoc):
-    topicIdentifier = Text()
+    topic_identifier = Text()
     title = Text()
     tags = Text()
 
@@ -31,17 +34,32 @@ class Extractor:
         return data
 
     def parse_data(self, input):
-        inputCalls = input['callData']['Calls']
-        print('Found {0} calls'.format(len(inputCalls)))
+        input_calls = input['callData']['Calls']
+        print('Found {0} calls'.format(len(input_calls)))
 
-        for inputCall in inputCalls:
-            outputCall = Call()
-            outputCall.callId = inputCall['CallIdentifier']['CallId']
-            outputCall.programme = inputCall['FrameworkProgramme']
-            outputCall.title = inputCall['Title']
-            outputCall.save()
-            print('Saved call {0}'.format(outputCall.callId))
+        for input_call in input_calls:
+            output_call = Call()
+            output_call.callId = input_call['CallIdentifier']['CallId']
+            output_call.programme = input_call['FrameworkProgramme']
+            output_call.title = input_call['Title']
 
+            input_call_budgets = input_call['CallBudgetOverview'][0].values()
+            for input_call_budget in input_call_budgets:
+                for input_topic in input_call_budget['Topic']:
+
+                    match = re.match(r'^(.+) - (.+)$', input_topic)
+
+                    if not match or len(match.groups()) != 2:
+                        print('Invalid topic title! {0}'.format(input_topic))
+                        continue
+
+                    output_topic = Topic()
+                    output_topic.topic_identifier = match.group(1)
+                    output_topic.title = match.group(2)
+                    output_call.topics.append(output_topic)
+
+            output_call.save()
+            print('Saved call {0}'.format(output_call.callId))
         print('All calls saved')
 
 
