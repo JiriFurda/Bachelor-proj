d026b065e55f500749825f2e739921b17527a282
Furdys <magiecz@gmail.com>
2019-03-13 20:51:09 +0100

    Basic component communication prototype

--- portal/static/js/vue.js
+++ portal/static/js/vue.js
@@ -23,7 +23,72 @@ Vue.component('sidebar-facet', {
     }
 });
 
+Vue.component('modal-facet-list', {
+    template: `<div><modal-facet v-for="(facet, index) in facets" :index="index"></modal-facet></div>`, // <div v-for="facet in store.facets">TEST</div><
+    computed: {
+        facets() {
+            return store.state.facets;
+        }
+    }
+});
 
+Vue.component('modal-facet', {
+    props: ['index'],
+    template: `
+    <div class="modal fade facet-modal" id="facetModal-test" tabindex="-1" role="dialog"  aria-hidden="true">
+         <div class="modal-dialog modal-lg" role="document">
+             <div class="modal-content">
+                 <div class="modal-header">
+                     <h5 class="modal-title">{{ facet.title }}</h5>
+                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                         <span aria-hidden="true">&times;</span>
+                     </button>
+                 </div>
+                 <div class="modal-body">
+                     <div class="in-facet-search">
+                         <input class="form-control form-control-sm in-facet-search-input">
+                     </div>
+                     <div class="pt-2 facet-modal-options">
+                         <div v-for="option in facet.checkedOptions" class="form-check w-100">
+                             <input type="checkbox" class="form-check-input" :value="option" :key="option.value" name="test" v-model="facet.checkedOptions">
+                             <label class="form-check-label w-100">
+                                 <div class="facet-submenu-item">
+                                     <div class="d-inline-block text-truncate">{{ option.text }}</div>
+                                     <div><span class="badge badge-pill badge-secondary">test</span></div>
+                                 </div>
+                             </label>
+                         </div>
+    
+                         <hr>
+    
+                         <div v-for="option in facet.modalOptions" class="form-check w-100">
+                             <input type="checkbox" class="form-check-input" :value="option" :key="option.value" name="test" v-model="facet.checkedOptions">
+                             <label class="form-check-label w-100">
+                                 <div class="facet-submenu-item">
+                                     <div class="d-inline-block text-truncate">{{ option.text }}</div>
+                                     <div><span class="badge badge-pill badge-secondary">test</span></div>
+                                 </div>
+                             </label>
+                         </div>
+                     </div>
+                 </div>
+                 <div class="modal-footer">
+                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                     <button type="button" class="btn btn-primary">Save changes</button>
+                 </div>
+             </div>
+         </div>
+     </div>
+                 `,
+    computed: {
+        facet() {
+            return store.state.facets[this.index];
+        }
+    }
+});
+
+
+/*
 Vue.component('facet-modal', {
     props: ['facet_key', 'facet_title', 'list'],
 
@@ -97,13 +162,10 @@ Vue.component('facet-modal', {
         }
     }
 });
+*/
 
 const store = new Vuex.Store({
     state: {
-        tasks: [
-            {task: 'aaa'},
-            {task: 'bbb'}
-        ],
         facets: [
             {
                 name: 'testfacet',
@@ -117,6 +179,7 @@ const store = new Vuex.Store({
                     {text: 'Slovakia', value: 'sk'},
                     {text: 'Netherlands', value: 'nl'}
                 ],
+                checkedOptions: [],
             },
         ],
     },
