e963bf71639b723fa5101d2921aabaf7fd7123c7
Furdys <magiecz@gmail.com>
2019-03-29 01:12:16 +0100

    Sidebar works

--- portal/controllers/search_controller.py
+++ portal/controllers/search_controller.py
@@ -4,6 +4,7 @@ from flask import Blueprint, request, abort, url_for, redirect, render_template
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from models.facet import Facet
+import json
 
 search_controller = Blueprint('search', __name__)
 client = Elasticsearch()
@@ -11,11 +12,18 @@ client = Elasticsearch()
 
 @search_controller.route('/')
 def index():
-    es_search_raw = buildSearch()
-    es_search = appendAggregations(es_search_raw)
+    es_search_raw = buildSearch()   # Raw query
+    es_search = appendAggregations(es_search_raw)   # Query with facets aggregations
+
+    es_response = es_search.execute()
+
+    layout_data = prepareLayoutData(es_search_raw, es_response)
 
 
-    return render_template('debug.html', debug=es_search.execute().aggregations.to_dict())
+    return render_template('debug.html',
+                           layout_data = layout_data,
+                           debug = es_search.execute().aggregations.to_dict()
+                           )
 
 def buildSearch():
     es_search = Search(using=client, index=['xstane34_projects', 'xstane34_deliverables'])
@@ -31,4 +39,33 @@ def appendAggregations(es_search):
         es_search.aggs.bucket(facet.name, 'terms', field=facet.field, size=6)
 
     return es_search
+
+def prepareLayoutData(es_search_raw, es_response):
+    facets = Facet.all()
+
+    vue_facets = ''
+    for facet in facets:
+        facet_dict = facet.toDict()
+        facet_dict.update({'checkedOptions': []})
+
+        aggregation = eval('es_response.aggregations.' + facet.name).buckets
+        option_list = []
+        for option in aggregation:
+            option_list.append({
+                'value': option.key,
+                'text': option.key,
+                'count': option.doc_count,
+            })
+        facet_dict.update({'mostFrequentOptions': option_list})
+
+        vue_facets += json.dumps(facet_dict)
+        vue_facets += ','
+    vue_facets = '[' + vue_facets[:-1] + ']'
+
+    result = {
+        'vue_facets': vue_facets,
+        'vue_elastic_search': json.dumps(es_search_raw.to_dict())
+    }
+
+    return result
 
