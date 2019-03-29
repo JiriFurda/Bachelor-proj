2f16a0047fc49bb6c91e66081d5c793a5e2e17cd
Furdys <magiecz@gmail.com>
2019-03-29 02:13:10 +0100

    Small fix

--- portal/controllers/search_controller.py
+++ portal/controllers/search_controller.py
@@ -29,7 +29,7 @@ def index():
 def buildSearch():
     es_search = Search(using=client, index=indices)
     es_search = es_search.query(
-        Q('query_string', query=request.args.get('query-new', 'test'),
+        Q('query_string', query=request.args.get('query', '*'),
           fields=['acronym^6', 'title^5', 'objective^3', 'fundedUnder.subprogramme^2', 'website.origWeb']))
 
     return es_search
