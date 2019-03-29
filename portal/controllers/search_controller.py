4c600cf426d7b575684ea27ab0e0480acf9c13cb
Furdys <magiecz@gmail.com>
2019-03-29 01:48:04 +0100

    result are splitted

--- portal/controllers/search_controller.py
+++ portal/controllers/search_controller.py
@@ -4,11 +4,11 @@ from flask import Blueprint, request, abort, url_for, redirect, render_template
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search, Q
 from models.facet import Facet
-import json
+import json, copy
 
 search_controller = Blueprint('search', __name__)
 client = Elasticsearch()
-
+indices = ['xstane34_projects', 'xstane34_deliverables']
 
 @search_controller.route('/')
 def index():
@@ -19,14 +19,15 @@ def index():
 
     layout_data = prepareLayoutData(es_search_raw, es_response)
 
+    results = groupResults(es_search_raw)
 
-    return render_template('debug.html',
-                           layout_data = layout_data,
-                           debug = es_search.execute().aggregations.to_dict()
+    return render_template('search/index.html',
+                           layout_data=layout_data,
+                           results=results,
                            )
 
 def buildSearch():
-    es_search = Search(using=client, index=['xstane34_projects', 'xstane34_deliverables'])
+    es_search = Search(using=client, index=indices)
     es_search = es_search.query(
         Q('query_string', query=request.args.get('query-new', 'test'),
           fields=['acronym^6', 'title^5', 'objective^3', 'fundedUnder.subprogramme^2', 'website.origWeb']))
@@ -66,6 +67,16 @@ def prepareLayoutData(es_search_raw, es_response):
         'vue_facets': vue_facets,
         'vue_elastic_search': json.dumps(es_search_raw.to_dict())
     }
+
+    return result
+
+def groupResults(es_search):
+    result = {}
+
+    for index in indices:
+        subsearch = copy.copy(es_search)
+        subsearch = subsearch.query(Q('term', _index=index))
+        result.update({index: subsearch.execute()})
 
     return result
 
